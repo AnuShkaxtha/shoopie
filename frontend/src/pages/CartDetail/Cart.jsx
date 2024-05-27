06400c9e05c1a595f6682340074d241586a159ac
AnuShkaxtha <xthanushka79@gmail.com>
2024-05-27 16:12:12 +0545

    mid

--- frontend/src/pages/CartDetail/Cart.jsx
+++ frontend/src/pages/CartDetail/Cart.jsx
@@ -80,51 +80,51 @@ const Cart = () => {
   };
 
   // Function to handle checkout
- // Function to handle checkout
- const handleCheckout = async () => {
-  setLoading(true);
-  try {
-    // Extract product details from the cart
-    const products = cart.map(item => ({
-      productName: item.name,
-      productId: item.id,
-      quantity: item.qnty,
-      // Add any additional product details required by your Strapi API
-    }));
+  // Function to handle checkout
+  const handleCheckout = async () => {
+    setLoading(true);
+    try {
+      // Extract product details from the cart
+      const products = cart.map((item) => ({
+        productName: item.name,
+        productId: item.id,
+        quantity: item.qnty,
+        // Add any additional product details required by your Strapi API
+      }));
 
-    // Prepare the order data
-    const orderData = {
-      products,
-      email: currentUser.email, 
-    };
+      // Prepare the order data
+      const orderData = {
+        products,
+        email: currentUser.email,
+      };
 
-    // Make API request to Strapi endpoint
-    const response = await fetch("http://localhost:1337/api/orders", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    
-      body: JSON.stringify({data:orderData}),
-    });
-    const responseData = await response.json();
+      // Make API request to Strapi endpoint
+      const response = await fetch("http://localhost:1337/api/orders", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
 
-    // Logging response data
-    console.log("Response:", responseData);
-    // Check if request was successful
-    if (response.ok) {
-      console.log("Order placed successfully!");
-      // Clear the cart after placing the order
-     // dispatch(clearCart());
-    } else {
-      console.error("Error placing order:", response.statusText);
+        body: JSON.stringify({ data: orderData }),
+      });
+      const responseData = await response.json();
+
+      // Logging response data
+      console.log("Response:", responseData);
+      // Check if request was successful
+      if (response.ok) {
+        console.log("Order placed successfully!");
+        // Clear the cart after placing the order
+        // dispatch(clearCart());
+      } else {
+        console.error("Error placing order:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error placing order:", error.message);
+    } finally {
+      setLoading(false);
     }
-  } catch (error) {
-    console.error("Error placing order:", error.message);
-  } finally {
-    setLoading(false);
-  }
-};
+  };
 
   return (
     <div
@@ -156,44 +156,49 @@ const Cart = () => {
                     <div key={item.id} className="pb-4 mb-4 border-b">
                       <div className="flex items-center justify-start ml-5">
                         {/* image */}
-                        <img
-                          src={imageUrl}
-                          alt={item.name}
-                          className="w-20 mr-4 h-25"
-                        />
-                        <div>
-                          <p className="text-lg font-bold">{item.name}</p>
-                          <p className="text-sm">Price: ${item.price}</p>
-                          <p className="text-sm">Quantity: {item.qnty}</p>
-                          <p className="text-sm">
+                        <div >
+                          <img
+                            src={imageUrl}
+                            alt={item.name}
+                            className="w-[150px] mr-4 h-25"
+                          />
+                        </div>
+
+                        <div className="mb-8 ml-4">
+                          <p className="mb-3 text-lg font-bold">{item.name}</p>
+                          <p className="mb-0.5 text-sm">Price: ${item.price}</p>
+                          <p className="mb-0.5 text-sm">
+                            Quantity: {item.qnty}
+                          </p>
+                          <p className="mb-0.5 text-sm">
                             Total: ${item.price * item.qnty}
                           </p>
+                          <div className="flex items-center">
+                            <Button
+                              variant="ghost"
+                              onClick={
+                                item.qnty <= 1
+                                  ? () => handleRemove(item.id)
+                                  : () => handleSingleDecrement(item.id)
+                              }
+                            >
+                              <Minus size={18} />
+                            </Button>
+                            <Button
+                              variant="ghost"
+                              onClick={() => handleIncrement(item.id)}
+                            >
+                              <Plus size={18} />
+                            </Button>
+                          </div>
                         </div>
-                      </div>
-                      <div className="flex items-center">
-                        <Button
-                          variant="ghost"
-                          onClick={
-                            item.qnty <= 1
-                              ? () => handleRemove(item.id)
-                              : () => handleSingleDecrement(item.id)
-                          }
-                        >
-                          <Minus />
-                        </Button>
-                        <Button
-                          variant="ghost"
-                          onClick={() => handleIncrement(item.id)}
-                        >
-                          <Plus />
-                        </Button>
                       </div>
                     </div>
                   );
                 })}
               </div>
-              <div className="flex justify-start mt-4 text-[15px]">
-                <div className="text-left">
+              <div className="flex justify-end mt-4 text-[15px]">
+                <div className="text-right">
                   <p className="mb-2">
                     Total Quantity:{" "}
                     <span className="font-bold">{totalQuantity}</span>
@@ -204,7 +209,7 @@ const Cart = () => {
                   </p>
                 </div>
               </div>
-              <div>
+              <div className="flex justify-end">
                 <Button onClick={handleCheckout} disabled={loading}>
                   {loading ? "Placing Order..." : "Proceed To Checkout"}
                 </Button>
