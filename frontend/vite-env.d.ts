722faa10da4930b5ac2b2423ea241c5a023775d4
AnuShkaxtha <xthanushka79@gmail.com>
2024-06-04 17:15:46 +0545

    migrating components to tsx

---
+++ frontend/vite-env.d.ts
@@ -0,0 +1,19 @@
+
+interface ImportMeta {
+    env:
+    { readonly VITE_REACT_APP_FIREBASE_API_KEY: string;
+        readonly VITE_REACT_APP_FIREBASE_AUTH_DOMAIN: string;
+        readonly VITE_REACT_APP_FIREBASE_PROJECT_ID: string;
+        readonly VITE_REACT_APP_FIREBASE_STORAGE_BUCKET: string;
+        readonly VITE_REACT_APP_FIREBASE_MESSAGING_SENDER: number;
+        readonly VITE_REACT_APP_FIREBASE_ID: string;
+        readonly VITE_REACT_APP_MEASUREMENT_ID: string;
+        // Add other environment vsariables here if needed
+  
+        };   
+      }
+  
+  // interface ImportMeta {
+  //   readonly env: ImportMetaEnv
+  //   }
+    
