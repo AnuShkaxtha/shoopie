56300f7785a63a6d216df64c76df1e8013d933d1
AnuShkaxtha <xthanushka79@gmail.com>
2024-06-04 14:44:30 +0545

    configuring firebase in ts

--- frontend/src/entities/firebase/firebase.ts
+++ frontend/src/entities/firebase/firebase.ts
@@ -1,29 +1,20 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, FirebaseApp } from "firebase/app";
-import { getAuth, Auth } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
-interface FirebaseConfig {
-    apiKey: string;
-    authDomain: string;
-    projectId: string;
-    storageBucket: string;
-    messagingSenderId: string;
-    appId: string;
-    measurementId?: string; // Optional
-}
 
-const firebaseConfig: FirebaseConfig = {
-    apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY as string,
-    authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN as string,
-    projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID as string,
-    storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET as string,
-    messagingSenderId: import.meta.env.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER as string,
-    appId: import.meta.env.VITE_REACT_APP_FIREBASE_ID as string,
-    measurementId: import.meta.env.VITE_REACT_APP_MEASUREMENT_ID as string
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER,
+    appId: import.meta.env.VITE_REACT_APP_FIREBASE_ID,
+    measurementId: import.meta.env.VITE_REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
-const app: FirebaseApp = initializeApp(firebaseConfig);
-const auth: Auth = getAuth(app);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 export { app, auth };
