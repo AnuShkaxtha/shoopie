60261a9ffd69727c6a4d19b687d9e0d0bbb9c1cd
AnuShkaxtha <xthanushka79@gmail.com>
2024-06-19 10:28:27 +0545

    npm install

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,12 @@
 'use strict';
+import cors from 'cors';
+import express from 'express';
+const app = express();
 
-module.exports = {
-  /**
-   * An asynchronous register function that runs before
-   * your application is initialized.
-   *
-   * This gives you an opportunity to extend code.
-   */
-  register(/*{ strapi }*/) {},
 
-  /**
-   * An asynchronous bootstrap function that runs before
-   * your application gets started.
-   *
-   * This gives you an opportunity to set up your data model,
-   * run jobs, or perform some special logic.
-   */
-  bootstrap(/*{ strapi }*/) {},
-};
+app.use(cors({
+  origin: 'https://your-netlify-domain.netlify.app'
+}));
+
+export function register( /*{ strapi }*/) { }
+export function bootstrap( /*{ strapi }*/) { }
