7a3926d01d22e69644fa716fade7330235b1117f
AnuShkaxtha <xthanushka79@gmail.com>
2024-07-01 13:04:06 +0545

    adding cancled option in order status

--- backend/types/generated/contentTypes.d.ts
+++ backend/types/generated/contentTypes.d.ts
@@ -885,7 +885,9 @@ export interface ApiOrderOrder extends Schema.CollectionType {
     products: Attribute.JSON;
     email: Attribute.Email;
     price: Attribute.Integer;
-    status: Attribute.Enumeration<['Pending', 'In Progress', 'Delivered']>;
+    status: Attribute.Enumeration<
+      ['Pending', 'In Progress', 'Delivered', 'Canceled']
+    >;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     publishedAt: Attribute.DateTime;
